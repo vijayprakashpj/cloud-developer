3cc4f107560c9392dbd72d20372cbd230255508d
Vijay Prakash P J <vijay2029@gmail.com>
2019-12-27 04:06:07 +0100

    Added Cars logic

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -70,13 +70,55 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get( "/cars/",
+    async ( req: Request, res: Response ) => {
+      const { make } = req.query;
+      
+      if ( !make ) {
+        return res.status(200)
+                  .send(cars.filter( (car, make) => car.make == make ));
+      } else {
+        return res.status(200)
+                  .send(cars);
+      }
+  } );
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get( "/cars/:id",
+    async ( req:Request, res:Response ) => {
+    const { id } = req.params;
+    
+    if ( !id ) {
+      return res.status(200)
+                .send(cars.filter( (car, id) => car.id == id ));
+    } else {
+      return res.status(400)
+                .send(`Id is required`);
+  });
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post( "/cars/",
+    async ( req:Request, res:Response) => {
+      const { id, type, model, cost } = req.body;
+    
+      if ( !id || !type || !model || !cost) {
+        return res.status(400)
+                  .send(`All the below attributes are required: \nId, Type, Model and Cost`);
+      } else {
+        const new_car:Car = new Car();
+        new_car.id = id;
+        new_car.type;
+        new_car.make = make;
+        new_car.cost = cost;
+        
+        cars.append(new_car);
+        
+        return res.status(201);
+      }
+  });
 
   // Start the Server
   app.listen( port, () => {
