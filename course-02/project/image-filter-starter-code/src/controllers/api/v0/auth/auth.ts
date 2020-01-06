25db126d70e319770a54d4448e6ba731033dc28c
Vijay Prakash P J <vijay2029@gmail.com>
2020-01-06 19:54:23 +0100

    Cloud Developer - Image filter API implementation (#5)
    * Image filter API implementation
    
    * Skip authentication for non-prod environments
    
    * Added screenshot of EB deployment
    
    * Added exception handling for invalid images/urls
    
    * Fixed unauthorized response HTTP error code
    
    * Updated postman collection
    
    * Updated postman collection - the api key parameter

---
+++ course-02/project/image-filter-starter-code/src/controllers/api/v0/auth/auth.ts
@@ -0,0 +1,23 @@
+import { NextFunction } from "connect";
+import { Request, Response } from "express";
+import { config } from "../../../../config/config";
+
+export function requiresAuth(req: Request, res: Response, next: NextFunction) {
+    const { api_key } = req.query;
+
+    // Skip authentication for non-Prod environments
+    if (config.env.toLowerCase() != 'prod') {
+        return next();
+    }
+
+    if (!api_key) {
+        return res.status(401).send("API key is missing.");
+    }
+
+    // Compare the API key values
+    if (api_key == config.api_key) {
+        return next();
+    } else {
+        return res.status(401).send("Not authorized.");
+    }
+}
